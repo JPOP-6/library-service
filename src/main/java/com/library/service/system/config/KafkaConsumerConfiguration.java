b91b8aeb30bdc2e9e9ddcfbf42442e92a8525d28
Prerna_Gandhi <prernagandhi97@gmail.com>
2021-07-06 20:09:23 +0530

    Week-4: Configuration of bootstrap server to point to ec2 instance running kafka server

--- src/main/java/com/library/service/system/config/KafkaConsumerConfiguration.java
+++ src/main/java/com/library/service/system/config/KafkaConsumerConfiguration.java
@@ -20,7 +20,7 @@ public class KafkaConsumerConfiguration {
     @Bean
     public ConsumerFactory<String, String > consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "ec2-18-117-105-0.us-east-2.compute.amazonaws.com:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
